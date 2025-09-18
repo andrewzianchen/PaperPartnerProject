a390f61bf1851ff078bc40dd916c6869cdfa7c5a
andrewzianchen <andrewzianchen@gmail.com>
2025-09-17 21:55:56 -0500

    Added a prompt to demo app.

--- SegmentedButtonsDemo/app/index.tsx
+++ SegmentedButtonsDemo/app/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import SegmentedButtonComponent from "./components/SegmentedButtonComponent";
 
 export default function Index() {
@@ -10,15 +10,21 @@ export default function Index() {
 
   return (
     <View style={styles.container}>
+      <Text style = {styles.text}
+      >Click the buttons to change the color of the box!
+      </Text>
       <SegmentedButtonComponent background={background} onChangeBackground={changeBackground}/>
-      <View style={[styles.box, {backgroundColor: background}]}>
-
-      </View>
+      <View style={[styles.box, {backgroundColor: background}]}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  text:{
+    fontSize: 30,
+    marginTop: 30,
+    fontWeight: "bold",
+  },
   container: {
     flex: 1,
     justifyContent: "center",
@@ -29,5 +35,5 @@ const styles = StyleSheet.create({
     height: 350,
     borderWidth: 2,
     marginBottom: 300,
-  }
+  },
 });
